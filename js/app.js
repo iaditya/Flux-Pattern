5fae92eb107ad02610cbf04b9a192e634f071f90
Aditya Gupta <iadityapansari@gmail.com>
2020-08-08 17:40:59 +0530

    adds event emitter

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 //entry point
+
+var emitter = new EventEmitter();
+
+emitter.on("STARTED", function () {
+  console.log("started app.");
+});
+
+emitter.on("STARTED", function () {
+  console.log("started app again.");
+});
+
+emitter.emit("STARTED");
+
 ReactDOM.render(<Forum />, document.getElementById("forum"));
