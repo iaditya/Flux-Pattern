cf1348b306ea89dba6ab189a68acc7b7a33d3d9b
Aditya <iadityapansari@gmail.com>
2024-08-17 17:39:05 +0530

    added cdn react + rendered first component.

---
+++ index.js
@@ -0,0 +1,6 @@
+const App = () => React.createElement("div", null, "Hello this is my react component.")
+
+ReactDOM.render(
+    React.createElement(App, null),
+    document.getElementById("app")
+)
